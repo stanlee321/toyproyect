9fa8ce591ece6835a701280324dd225410554306
Stanley Salvatierra <stanlee321@gmail.com>
2017-11-06 18:13:21 -0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='sistema de manejo para consultorios',
+    version='2.7',
+    packages=['package'],
+    url='',
+    license='',
+    author='Tushar',
+    author_email='stanlee321@gmail.com',
+    description='HospitalManagementSystem writed in python 2.7 and flask'
+)
